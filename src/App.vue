8051d3837796e37b8cee348b26fa7d506e50e23c
ryomashita <86085692+ryomashita@users.noreply.github.com>
2023-05-05 11:08:37 +0900

    add template; rgb, ycbcr, color-div

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,62 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div>
+    <div>
+      <label for="red-input">R:</label>
+      <input type="number" id="red-input" v-model="rgb.red" @input="updateYCbCr" />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+      <label for="green-input">G:</label>
+      <input type="number" id="green-input" v-model="rgb.green" @input="updateYCbCr" />
+    </div>
+    <div>
+      <label for="blue-input">B:</label>
+      <input type="number" id="blue-input" v-model="rgb.blue" @input="updateYCbCr" />
+    </div>
+    <div>
+      <label for="y-input">Y:</label>
+      <input type="number" id="y-input" v-model="ycbcr.y" @input="updateRGB" />
+    </div>
+    <div>
+      <label for="cb-input">Cb:</label>
+      <input type="number" id="cb-input" v-model="ycbcr.cb" @input="updateRGB" />
+    </div>
+    <div>
+      <label for="cr-input">Cr:</label>
+      <input type="number" id="cr-input" v-model="ycbcr.cr" @input="updateRGB" />
+    </div>
+    <div :style="{ backgroundColor: rgbColor, width: '100px', height: '100px' }"></div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      rgb: { red: 0, green: 0, blue: 0 },
+      ycbcr: { y: 16, cb: 128, cr: 128 },
+      rgbColor: "rgb(0, 0, 0)",
+    };
+  },
+  methods: {
+    updateColor() {
+      this.color = `rgb(${this.redValue}, ${this.greenValue}, ${this.blueValue})`;
+    },
+    updateYCbCr() {
+      const { red, green, blue } = this.rgb;
+      const y = 0.183 * red + 0.614 * green + 0.062 * blue + 16;
+      const cb = -0.101 * red - 0.339 * green + 0.439 * blue + 128;
+      const cr = 0.439 * red - 0.399 * green - 0.040 * blue + 128;
+      this.ycbcr = { y, cb, cr };
+      this.rgbColor = `rgb(${red}, ${green}, ${blue})`;
+    },
+    updateRGB() {
+      const { y, cb, cr } = this.ycbcr;
+      const red = 1.164 * (y - 16) + 1.793 * (cr - 128);
+      const green = 1.164 * (y - 16) - 0.213 * (cb - 128) - 0.533 * (cr - 128);
+      const blue = 1.164 * (y - 16) + 2.112 * (cb - 128);
+      this.rgb = { red, green, blue };
+      this.rgbColor = `rgb(${red}, ${green}, ${blue})`;
+    },
+  },
+};
+</script>
